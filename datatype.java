773dd99f18267d6ab80fc183557039d90e1d994c
Sujnya <sujnyashetti@gmail.com>
2025-04-04 11:05:01 +0530

    save

---
+++ datatype.java
@@ -0,0 +1,17 @@
+package ciesujnya;
+
+public class datatype {
+    public static void main(String[] args){
+        int a=10;
+        float b=5.6f;
+        boolean c=true;
+        char d='A';
+        String e="My name is sujnya";
+
+        System.out.println("Integer: "+a);
+        System.out.println("Float: "+b);
+        System.out.println("Boolean: "+c);
+        System.out.println("character: "+d);
+        System.out.println("string: "+e);
+    }
+}
